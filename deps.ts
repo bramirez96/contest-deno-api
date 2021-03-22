b604504497c1b42b7ac3fcebae8ad88f7592d074
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-03-22 13:11:58 -0700

    Added in missing deps.

--- deps.ts
+++ deps.ts
@@ -13,7 +13,12 @@ export type { SendEmailRequest } from 'https://deno.land/x/aws_sdk@v0.0.1/client
 export * as bcrypt from 'https://deno.land/x/bcrypt@v0.2.4/mod.ts';
 // clever
 export { CleverClient } from 'https://deno.land/x/clever_sso_deno@v0.0.42/mod.ts';
-export type { ICleverUserInfo } from 'https://deno.land/x/clever_sso_deno@v0.0.42/mod.ts';
+export type {
+  CleverGradeType,
+  CleverSubjectType,
+  ICleverProfile,
+  ICleverUserInfo,
+} from 'https://deno.land/x/clever_sso_deno@v0.0.42/mod.ts';
 // cors @1.2.1
 export { opineCors } from 'https://deno.land/x/cors@v1.2.1/mod.ts';
 // Cotton v0.7.5
