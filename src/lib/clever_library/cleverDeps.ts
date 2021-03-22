b0e98f5429a4b9ed53490f336c93846a47b210ff
Brandon Ramirez <bran.ramirez.don@gmail.com>
2021-03-22 13:05:45 -0700

    Fixed liv issues.

--- src/lib/clever_library/cleverDeps.ts
+++
@@ -1,6 +0,0 @@
-// axiod @0.20.0-0
-import axiod from 'https://deno.land/x/axiod@0.20.0-0/mod.ts';
-export { axiod };
-
-// bb64 @ 1.1.0
-export { Base64 } from 'https://deno.land/x/bb64@1.1.0/mod.ts';
