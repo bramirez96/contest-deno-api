a01439875a8975d7e558150742e5c12ba8ea9def
vdeb <vdebebe924@gmail.com>
2021-03-31 23:33:58 -0400

    re-frame the fdbkassign.py, uses less space and is more effective also fixed bug where dict was using the same value for each key

--- src/api/routes/rumble/fbdkassign.py
+++ src/api/routes/rumble/fbdkassign.py
@@ -1,38 +1,25 @@
 import random
 
 #mock list for trial and error
-ISubItem = [3452, 3647, 3948, 3829, 3928, 3726]
-studentid = [93049, 90384, 93740, 94873, 94728, 92516]
+ISubItem = [3452, 3647, 3948, 3829, 3928, 3726, 3958, 3792, 3190, 3523, 3409, 3822]
+studentid = [93049, 90384, 93740, 94873, 94728, 92516, 93647, 92627, 91738, 93782, 93722, 93627]
 
 submissions = ISubItem #iSubItem, object list
-results1 = []
-results2 = []
-results3 = []
-#creates 3 seperate sets with 1,2,3 added
-for i, j in enumerate(submissions):
-    assignid1 = str(submissions[i]) + ('1')
-    results1.append(assignid1)
-    assignid2 = str(submissions[i]) + ('2')
-    results2.append(assignid2)
-    assignid3 = str(submissions[i]) + ('3')
-    results3.append(assignid3)
-
-# print(results1, results2, results3)
-
 
 #randomly shuffle lists 2 and 3
-shuf_res1 = results1
-shuf_res2 = random.sample(results2, len(results2))
-shuf_res3 = random.sample(results3, len(results3))
-# print(shuf_res1, shuf_res2, shuf_res3)
+shuffle1 = random.sample(submissions, len(submissions))
+shuffle2 = random.sample(shuffle1, len(shuffle1))
+shuffle3 = random.sample(shuffle2, len(shuffle2))
+print('shuffled1:',shuffle1, 'shuffled2:',shuffle2, 'shuffled3:', shuffle3)
 
 #creates lists with 1,2,3 combined for use as value pairs for student id key
-nested = [[i,j,k] for i,j,k in zip((shuf_res1), (shuf_res2), (shuf_res3))]
+nested = [[i,j,k] for i,j,k in zip((shuffle1), (shuffle2), (shuffle3))]
 # print(nested)
 
-
 fdbkassigned = {}
-for i in studentid:
-    for j in nested:
-        fdbkassigned[i]=j
+for key in studentid:
+    for value in nested:
+        fdbkassigned[key] = value
+        nested.remove(value)
+        break  
 print(fdbkassigned)
