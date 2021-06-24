01890f175c32f6f462c1089f2483e2bedba0d1ea
Brandon <bran.ramirez.don@gmail.com>
2021-06-24 13:41:20 -0700

    Small Redis loader fixes

--- src/loaders/redis.ts
+++ src/loaders/redis.ts
@@ -1,13 +1,13 @@
-import { redisConnect } from '../../deps.ts';
+import { Redis, redisConnect } from '../../deps.ts';
 import env from '../config/env.ts';
 
 export default () => {
   console.log('Loading Redis client...');
 
-  let redis;
+  let redis: Promise<Redis>;
   if (!env.REDIS_CONFIG.hostname && !env.REDIS_CONFIG.port) {
     console.log('Redis is not configured in your environment');
-    redis = {};
+    redis = Promise.resolve({}) as Promise<Redis>;
   } else if (env.DENO_ENV === 'testing') {
     redis = redisConnect(env.REDIS_CONFIG);
   } else {
