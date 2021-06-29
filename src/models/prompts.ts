2100c8b6c9ffcac07b4d56727d0ee055c7493705
Brandon <bran.ramirez.don@gmail.com>
2021-06-28 22:10:10 -0700

    Added limit to prompt queue

--- src/models/prompts.ts
+++ src/models/prompts.ts
@@ -13,21 +13,12 @@ export default class PromptModel extends BaseModel<INewPrompt, IPrompt> {
       const prompts = ((await this.db
         .table('prompt_queue')
         .innerJoin('prompts', 'prompts.id', 'prompt_queue.promptId')
+        .limit(7)
         .where('prompt_queue.starts_at', Q.gte(moment.utc().format()))
         .select('prompts.*', 'prompt_queue.starts_at')
         .execute()) as unknown[]) as IPromptInQueue[];
 
-      // Return early if we already have the active prompt
-      if (prompts.some((p) => p.active)) return prompts;
-
-      const [currentPrompt] = ((await this.db
-        .table('prompt_queue')
-        .innerJoin('prompts', 'prompts.id', 'prompt_queue.promptId')
-        .where('prompts.active', true)
-        .select('prompts.*', 'prompt_queue.starts_at')
-        .execute()) as unknown[]) as IPromptInQueue[];
-
-      return [currentPrompt, ...prompts];
+      return prompts;
     } catch (err) {
       this.logger.error(err);
       throw err;
