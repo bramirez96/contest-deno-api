ac7107bc22d2ed20debf1e7a0dcd05bfced3b210
Brandon <bran.ramirez.don@gmail.com>
2021-06-29 11:32:51 -0700

    Fixed current prompt issue

--- src/models/prompts.ts
+++ src/models/prompts.ts
@@ -17,8 +17,17 @@ export default class PromptModel extends BaseModel<INewPrompt, IPrompt> {
         .where('prompt_queue.starts_at', Q.gte(moment.utc().format()))
         .select('prompts.*', 'prompt_queue.starts_at')
         .execute()) as unknown[]) as IPromptInQueue[];
+      // Return early if we already have the active prompt
+      if (prompts.some((p) => p.active)) return prompts;
 
-      return prompts;
+      const [currentPrompt] = ((await this.db
+        .table('prompt_queue')
+        .innerJoin('prompts', 'prompts.id', 'prompt_queue.promptId')
+        .where('prompts.active', true)
+        .select('prompts.*', 'prompt_queue.starts_at')
+        .execute()) as unknown[]) as IPromptInQueue[];
+
+      return [currentPrompt, ...prompts];
     } catch (err) {
       this.logger.error(err);
       throw err;
